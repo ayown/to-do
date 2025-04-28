07fc18bd0246e677d05d50dbb4e3c21566e2b854
AYAN BHATTACHARJEE <bhattacharjeeayan93@gmail.com>
2025-04-29 01:57:32 +0530

    Added To-Do

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export function TodoInput(props) {
+  const { handleAddTodo } = props
+
+  return (
+    <div className="todo-input">
+      <input 
+        type="text" 
+        placeholder="Add a new todo..."
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            handleAddTodo(e.target.value)
+            e.target.value = ''
+          }
+        }}
+      />
+    </div>
+  )
+}
